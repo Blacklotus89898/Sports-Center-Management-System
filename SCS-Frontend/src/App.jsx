453ce60d4f699003b5cea2e7516f16805eaf574d
q <30962319+lectern@users.noreply.github.com>
2024-02-23 14:43:16 -0500

    init frontend

---
+++ SCS-Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Temp from './pages/Temp'
+import NoPage from './pages/NoPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/temp" element={<Temp />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ SCS-Frontend/src/pages/Temp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Temp() {
+    return (
+        <div>
+            <h1>Temp</h1>
+            <p>This is a temporary page.</p>
+        </div>
+    );
+}
