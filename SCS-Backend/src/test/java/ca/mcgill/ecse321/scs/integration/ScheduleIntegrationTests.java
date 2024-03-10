2b8bbbc8bfa393ab9d4f25dabee03c8a29e6c8d5
q <30962319+lectern@users.noreply.github.com>
2024-03-09 21:55:44 -0500

    endpoint excpetion handler (no server error!!!) omg

---
+++ SCS-Backend/src/test/java/ca/mcgill/ecse321/scs/integration/ScheduleIntegrationTests.java
@@ -0,0 +1,24 @@
+package ca.mcgill.ecse321.scs.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ScheduleIntegrationTests {
+    
+}
