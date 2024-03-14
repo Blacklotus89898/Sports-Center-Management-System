f9eb6f3485de9a014fecc8bc32c3e02e4216f034
Blacklotus89898 <song.yang.chen89898@gmail.com>
2024-03-13 21:58:38 -0400

    changed all the service for id querying and Scs exception accomodation

--- SCS-Backend/src/main/java/ca/mcgill/ecse321/scs/service/CustomerService.java
+++ SCS-Backend/src/main/java/ca/mcgill/ecse321/scs/service/CustomerService.java
@@ -1,80 +1,96 @@
 package ca.mcgill.ecse321.scs.service;
 
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.scs.dao.CustomerRepository;
-import ca.mcgill.ecse321.scs.dao.InstructorRepository;
-import ca.mcgill.ecse321.scs.dao.OwnerRepository;
+import ca.mcgill.ecse321.scs.exception.SCSException;
 import ca.mcgill.ecse321.scs.model.Customer;
-import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
-@Transactional //everything interating with db --will be moved on top of each if other non-transactional functions
+@Transactional // everything interating with db --will be moved on top of each if other
+               // non-transactional functions
 @Service
 public class CustomerService {
-    
+
     @Autowired
     private CustomerRepository customerRepository;
-    @Autowired
-    private InstructorRepository instructorRepository;
-    @Autowired
-    private OwnerRepository ownerRepository;
-    
+    // @Autowired
+    // private InstructorRepository instructorRepository;
+    // @Autowired
+    // private OwnerRepository ownerRepository;
+    // above commented are used once implementation are done
 
-    // public Iterable<Customer> findAllCustomers() {
-    //     return (ArrayList<Customer>) customerRepository.findAll();
-    // }
+    public List<Customer> getAllCustomer() {
+        return ServiceUtils.toList(customerRepository.findAll());
+    }
+
+    public Customer getCustomerById(Integer id) {
+        Optional<Customer> customer = customerRepository.findById(id.toString());
+        if (!customer.isPresent())
+            throw new SCSException(HttpStatus.NOT_FOUND, ("Customer not found with id: " + id));
+        return customer.get();
+    }
+
+    public Customer getCustomerByEmail(String customerEmail) {
+        Customer customer = customerRepository.findCustomerByEmail(customerEmail);
+        if (customer == null)
+            throw new EntityNotFoundException(String.format("Customer not found with email: %s", customerEmail));
+        return customer;
+    }
 
     public Customer createCustomer(String name, String email, String password) {
-        // error checking --input sanitation are done in controller
-        if (password.length() > 100 ) throw new IllegalArgumentException(String.format("Invalid password: %s", password));
-        else if ((customerRepository.findCustomerByEmail(email) != null) 
+        if ((customerRepository.findCustomerByEmail(email) != null)
         // || instructorRepository.findInstructorByEmail(email)!=null
-        // || ownerRepository.findOwnerByEmail(email)!=null
-        ) {
-            throw new EntityExistsException(String.format("Email account already exists: %s", email));
-            // create the customer object here
+        // || ownerRepository.findOwnerByEmail(email)!=null)
+        ) { // the previous repo are not done yet
+            throw new SCSException(HttpStatus.CONFLICT, "Email account already exists:" + email);
         }
-        // to be improved later
+
         Customer newCustomer = new Customer();
         newCustomer.setName(name);
         newCustomer.setPassword(password);
         newCustomer.setEmail(email);
-        newCustomer.setCreationDate(null);
+        newCustomer.setCreationDate(new Date(System.currentTimeMillis()));
         customerRepository.save(newCustomer);
-        return newCustomer; //for test sake
-    }
-    
-    // Read --will be deleted
-    // public Customer findCustomerById(String customerId) {
-    //      Optional<Customer> customer = customerRepository.findById(customerId);
-    //      if (customer.isPresent()) return customer.get();
-    //      throw new EntityNotFoundException(String.format("Customer not found with id: %s", customerId));
-    // }
-
-    // will renamed to get
-    public Customer getCustomerByEmail(String customerEmail) {
-         Customer customer = customerRepository.findCustomerByEmail(customerEmail);
-         if (customer == null) throw new EntityNotFoundException(String.format("Customer not found with email: %s", customerEmail));
-         return customer;
+        return newCustomer; // for test sake
     }
 
-    // Update --need to check if using dto --> parsed inside the controller
-    public Customer updateCustomerByEmail(Customer customer) {
-        // error checking 
-        Customer currentCustomer = getCustomerByEmail(customer.getEmail());
-        // currentCustomer.setEmail(customer.getEmail());
-        currentCustomer.setName(customer.getName());
-        currentCustomer.setPassword(customer.getPassword());
+    public Customer updateCustomerById(Integer id, String name, String email, String password) {
+        Optional<Customer> optionCustomer = customerRepository.findById(id.toString());
+        if (!optionCustomer.isPresent())
+            throw new SCSException(HttpStatus.NOT_FOUND, ("Customer not found with id: " + id));
+
+        Customer currentCustomer = optionCustomer.get();
+        currentCustomer.setEmail(email); // email should never change unless provided the option
+        currentCustomer.setName(name);
+        currentCustomer.setPassword(password);
         return customerRepository.save(currentCustomer);
     }
 
-    // Delete
-    public void deleteCustomerByEmail(String customerEmail) {
-        customerRepository.delete(getCustomerByEmail(customerEmail));
+    // public Customer updateCustomerByEmail(Customer customer) {
+    // // error checking in controller unless different implementation
+    // Customer currentCustomer =
+    // customerRepository.findCustomerByEmail(customer.getEmail());
+    // // currentCustomer.setEmail(customer.getEmail()); --email should never change
+    // unless provided the option
+    // currentCustomer.setName(customer.getName());
+    // currentCustomer.setPassword(customer.getPassword());
+    // return customerRepository.save(currentCustomer);
+    // }
+
+    public void deleteCustomerById(Integer customerId) {
+        customerRepository.delete(getCustomerById(customerId));
     }
 
+    // public void deleteCustomerByEmail(String customerEmail) {
+    // customerRepository.delete(getCustomerByEmail(customerEmail));
+    // }
 
 }
