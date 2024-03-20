4b0e64dedf7aabf97630feb2b47ba351cf2216f9
connortate <connor.tate@mail.mcgill.ca>
2024-03-20 16:22:18 -0400

    Started the TeachingInfoIntegrationTests

--- SCS-Backend/src/main/java/ca/mcgill/ecse321/scs/controller/TeachingInfoController.java
+++ SCS-Backend/src/main/java/ca/mcgill/ecse321/scs/controller/TeachingInfoController.java
@@ -25,7 +25,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @CrossOrigin(origins = "*")
 @RestController
-@Tag(name = "TeachingInfo")
+@Tag(name = "TeachingInfo", description = "The TeachingInfo API")
 public class TeachingInfoController {
     // @Autowired
     // private TeachingInfoService teachingInfoService;
