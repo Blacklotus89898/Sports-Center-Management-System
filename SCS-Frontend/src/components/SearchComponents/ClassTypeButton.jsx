8a568609b41128ae306a9e5ca9091c35d76994a6
q <30962319+lectern@users.noreply.github.com>
2024-04-06 17:21:23 -0400

    redirect on classTYpe clyck

--- SCS-Frontend/src/components/SearchComponents/ClassTypeButton.jsx
+++ SCS-Frontend/src/components/SearchComponents/ClassTypeButton.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ClassTypeButton({ classTypeName, classTypeIcon,  onClick }) {
+    const navigate = useNavigate();
+
     return (
         <div
             className="flex flex-col p-2 justify-center items-center whitespace-nowrap hover:cursor-pointer"
+            onClick={() => {navigate("/search?classType=" + classTypeName)}}
         >   
             {/* icon */}
             <span className="mr-1">{classTypeIcon}</span>
