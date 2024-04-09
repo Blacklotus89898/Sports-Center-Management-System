105d9aa0d79c9eca4b80b642dcf25dcc6570280c
Henry Huang <64996660+G1ngerAle@users.noreply.github.com>
2024-04-09 12:50:25 -0400

    add the create function (the whole file need to be reviewed)

--- SCS-Frontend/src/components/DashboardComponents/ProfileComponents/Profile.jsx
+++ SCS-Frontend/src/components/DashboardComponents/ProfileComponents/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import useFetch from "../../../api/useFetch";
+import Modal from "../../Modal";
 
 import visaLogo from "../../../components/DashboardComponents/ProfileComponents/VISA.png";
 import amexLogo from "../../../components/DashboardComponents/ProfileComponents/AMEX.png";
@@ -74,25 +75,31 @@ export default function Profile() {
                 },
                 body: JSON.stringify({
                     // Include all necessary fields your API expects
-                    cardNumber: paymentMethod.cardNumber,
-                    expiryMonth: paymentMethod.expiryMonth,
-                    expiryYear: paymentMethod.expiryYear,
-                    securityCode: paymentMethod.securityCode,
+                    cardNumber: addCardNumber,
+                    expiryMonth: addExpiryMonth,
+                    expiryYear: addExpiryYear,
+                    securityCode: addSecurityCode,
                 })
             }, (newPaymentMethod) => {
                 setFetching(false);
                 if (newPaymentMethod) {
+                    setPaymentMethod([...paymentMethod, newPaymentMethod]);
+                    setAddCardNumber('');
+                    setAddExpiryMonth('');
+                    setAddExpiryYear('');
+                    setAddSecurityCode('');
+                    reset();
                     setHasPaymentMethod(true);
-                    // Update the state with the new payment method info here
-                    setPaymentInfo(newPaymentMethod);
                 }
             });
         } catch (error) {
             setFetching(false);
             setErrorMessage(error.response?.data?.message || 'An error occurred');
         }
-        console.log(paymentMethod.cardNumber, paymentMethod.expiryMonth, paymentMethod.expiryYear, paymentMethod.securityCode);
+
+        console.log(addCardNumber, addExpiryMonth, addExpiryYear, addSecurityCode);
     }
+    
 
     async function updatePaymentMethod(paymentMethod) {
         setErrorMessage('');
@@ -127,16 +134,98 @@ export default function Profile() {
     function FormatAddContent() {
         return (
             <>
-                {/* Input fields for adding a new payment method */}
-                {/* Include inputs for card number, expiry date, CVV, etc. */}
-                <div className="py-2" />
-                {error && <div className='py-1 text-error text-center'>{data?.errors?.toString()}</div>}
-                <button
-                    className="btn btn-primary"
-                    onClick={addPaymentMethod}
-                >
-                    Create Payment Method
-                </button>
+                <div className="w-full min-h-screen bg-white flex justify-center items-center p-8">
+                    <div className="w-full max-w-xl">
+                        <h1 className="text-xl font-bold mb-4">Payment Method</h1>
+                        <form onSubmit={handleSubmit} className="space-y-6">
+                            <div>
+                                <label className="text-gray-700 font-semibold block mb-2">
+                                    Please enter your card number. Do not use spaces.
+                                </label>
+                                <div className="flex justify-between items-center">
+                                    <input
+                                        type="text"
+                                        name="cardNumber"
+                                        setValue={setAddCardNumber}
+                                        className="w-full rounded-md border-gray-300"
+                                        placeholder="1234 5678 9012 3456"
+                                        textfield={true}
+                                    />
+                                    <span className="ml-4 flex items-center">
+                                        <img src={visaLogo} alt="Visa" className="h-3 mr-2" />
+                                        <img src={amexLogo} alt="Visa" className="h-3 mr-2" />
+                                        <img src={masterLogo} alt="Visa" className="h-3 mr-2" />
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="flex gap-4">
+                                <div>
+                                    <label className="text-gray-700 font-semibold block mb-2">
+                                        Expiry Date
+                                    </label>
+                                    <div className="flex gap-2">
+                                        <input
+                                            type="text"
+                                            name="expiryMonth"
+                                            setValue={setAddExpiryMonth}
+                                            className="rounded-md border-gray-300 w-20"
+                                            placeholder="MM"
+                                            textfield={true}
+                                        />
+                                        <input
+                                            type="text"
+                                            name="expiryYear"
+                                            setValue={setAddExpiryYear}
+                                            className="rounded-md border-gray-300 w-32"
+                                            placeholder="YYYY"
+                                            textfield={true}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <label className="text-gray-700 font-semibold block mb-2">
+                                    CVV
+                                </label>
+                                <div className="flex items-center">
+                                    <input
+                                        type="text"
+                                        name="securityCode"
+                                        setValue={setAddSecurityCode}
+                                        className="rounded-md border-gray-300 w-16"
+                                        placeholder="123"
+                                        textfield={true}
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowCVVTooltip(true)}
+                                        className="ml-2"
+                                    >
+                                        <img src={questionLogo} alt="?" className="h-4 mr-2" />
+                                    </button>
+                                    {showCVVTooltip && (
+                                        <div
+                                            ref={tooltipRef}
+                                            className="absolute p-1 bg-white border rounded shadow-lg"
+                                        >
+                                            <img src={CVVLogo} alt="CVV Information" />
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="py-2" />
+                            {errorMessage && <div className="text-red-500">{errorMessage}</div>}
+                            <div className="text-right">
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => {addPaymentMethod(); setCurrentFocus('')}}
+                                >
+                                    Create
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </>
         );
     }
@@ -237,9 +326,17 @@ export default function Profile() {
                             <div className="text-right">
                                 <button
                                     className="btn btn-primary"
-                                    onClick={() => updatePaymentMethod(paymentMethod)}
+                                    onClick={() => {
+                                        updatePaymentMethod({
+                                            cardNumber: updateCardNumber,
+                                            expiryMonth: updateExpiryMonth,
+                                            expiryYear: updateExpiryYear,
+                                            securityCode: updateSecurityCode
+                                        });
+                                        setCurrentFocus(category.className);
+                                    }}
                                 >
-                                    Update Payment Method
+                                    Update
                                 </button>
                             </div>
                         </form>
@@ -248,102 +345,20 @@ export default function Profile() {
             </>
         );
     }
-
-
     return (
-        <div className="w-full min-h-screen bg-white flex justify-center items-center p-8">
-            <div className="w-full max-w-xl">
-                <h1 className="text-xl font-bold mb-4">Payment Method</h1>
-                <form onSubmit={handleSubmit} className="space-y-6">
-                    <div>
-                        <label className="text-gray-700 font-semibold block mb-2">
-                            Please enter your card number. Do not use spaces.
-                        </label>
-                        <div className="flex justify-between items-center">
-                            <input
-                                type="text"
-                                name="cardNumber"
-                                value={paymentInfo.cardNumber}
-                                onChange={handleInputChange}
-                                className="w-full rounded-md border-gray-300"
-                                placeholder="1234 5678 9012 3456"
-                            />
-                            <span className="ml-4 flex items-center">
-                                <img src={visaLogo} alt="Visa" className="h-3 mr-2" />
-                                <img src={amexLogo} alt="Visa" className="h-3 mr-2" />
-                                <img src={masterLogo} alt="Visa" className="h-3 mr-2" />
-                            </span>
-                        </div>
-                    </div>
-                    <div className="flex gap-4">
-                        <div>
-                            <label className="text-gray-700 font-semibold block mb-2">
-                                Expiry Date
-                            </label>
-                            <div className="flex gap-2">
-                                <input
-                                    type="text"
-                                    name="expiryMonth"
-                                    value={paymentInfo.expiryMonth}
-                                    onChange={handleInputChange}
-                                    className="rounded-md border-gray-300 w-20"
-                                    placeholder="MM"
-                                />
-                                <input
-                                    type="text"
-                                    name="expiryYear"
-                                    value={paymentInfo.expiryYear}
-                                    onChange={handleInputChange}
-                                    className="rounded-md border-gray-300 w-32"
-                                    placeholder="YYYY"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <label className="text-gray-700 font-semibold block mb-2">
-                            CVV
-                        </label>
-                        <div className="flex items-center">
-                            <input
-                                type="text"
-                                name="securityCode"
-                                value={paymentInfo.securityCode}
-                                onChange={handleInputChange}
-                                className="rounded-md border-gray-300 w-16"
-                                placeholder="123"
-                            />
-                            <button
-                                type="button"
-                                onClick={() => setShowCVVTooltip(true)}
-                                className="ml-2"
-                            >
-                                <img src={questionLogo} alt="?" className="h-4 mr-2" />
-                            </button>
-                            {showCVVTooltip && (
-                                <div
-                                    ref={tooltipRef}
-                                    className="absolute p-1 bg-white border rounded shadow-lg"
-                                >
-                                    <img src={CVVLogo} alt="CVV Information" />
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                    <div className="py-2" />
-                    {error && <div className='py-1 text-error text-center'>{data?.errors?.toString()}</div>}
-                    <div className="text-right">
-                        <button
-                            type="submit"
-                            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        >
-                            {hasPaymentMethod
-                                ? "Update Payment Method"
-                                : "Create Payment Method"}
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
+        <>
+            {/* fetching loading */}
+            {fetching &&
+                <div className="flex w-full justify-center content-center">
+                    <span className="loading loading-ring loading-lg" />
+                </div>
+            }
+    
+            {/* payment method */}
+            {FormatUpdateContent(paymentMethod)}
+
+            {/* add item modal */}
+            {FormatAddContent()}
+        </>
     );
 }
