544fdf14d8defd5c74b4fc693f15efb9f18e1566
Henry Huang <64996660+G1ngerAle@users.noreply.github.com>
2024-04-09 14:34:14 -0400

    update app.jsx, add some pages

--- SCS-Frontend/src/App.jsx
+++ SCS-Frontend/src/App.jsx
@@ -9,6 +9,10 @@ import Search from './pages/Search'
 import Demo from './components/SchedulerComponents/Scheduler'
 import Dashboard from './pages/Dashboard'
 import Calendar from './components/SchedulerComponents/Calender'
+import AboutUs from './pages/AboutUs'
+import Career from './pages/Career'
+import ContactUs from './pages/ContactUs'
+import PressKit from './pages/PressKit'
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
             <Routes>
                 <Route index element={<Search />} />
                 <Route path="/temp" element={<Temp />} />
+                <Route path="/aboutus" element={<AboutUs />} />
+                <Route path="/career" element={<Career />} />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/presskit" element={<PressKit />} />
                 <Route path="/themes" element={<Themes />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/schedule" element={<Demo />} />
